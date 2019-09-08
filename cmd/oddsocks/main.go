20c8a1ce59f6853f065abbeaa5ac31ea9d28e81e
sransara <git@sransara.com>
2019-09-08 06:18:46 -0700

    oddsocks command

---
+++ cmd/oddsocks/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/sransara/odd-socks/pkg/oddsocks"
+)
+
+const CONN_HOST = "localhost:3030"
+
+func main() {
+	var laddr string
+	flag.StringVar(&laddr, "laddr", ":0", "Specify host:port to listen for incoming SOCKS connections")
+
+	listener, err := net.Listen("tcp", laddr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Listening on " + listener.Addr().String())
+
+	ctx := context.Background()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		go serveConnection(ctx, conn)
+	}
+}
+
+func serveConnection(ctx context.Context, conn net.Conn) {
+	defer conn.Close()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	currHandler := oddsocks.HandleAuthNegotiation
+	for {
+		currHandler, err := currHandler(ctx, conn)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if currHandler == nil {
+			break
+		}
+	}
+}
